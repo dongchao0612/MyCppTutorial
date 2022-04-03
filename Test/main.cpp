b295fa36977fd06343923d999a1b423440eaa180
dongchao0612 <3224318157@qq.com>
2022-04-03 16:55:58 +0800

    commit

--- Test/main.cpp
+++ Test/main.cpp
@@ -2,32 +2,36 @@
 
 using namespace std;
 
-class Base1 {
+class Base0 {
 public:
-	Base1(int i) { cout << "constructing Base1\t" << i << endl; }
-	~Base1() { cout << "Destructing Base1\t" << endl; }
+	Base0(int var):var0(var){}
+	int var0;
+	void fun0() {
+		cout << "member of base0" << endl;
+	}
 };
-class Base2 {
+class Base1 :virtual public Base0 {
 public:
-	Base2(int j) { cout << "constructing Base2\t" << j << endl; }
-	~Base2() { cout << "Destructing Base2\t" << endl; }
+	Base1(int var) :Base0(var){}
+	int var1;
 };
-class Base3 {
+class Base2 :virtual public Base0 {
 public:
-	Base3() { cout << "constructing Base3\t*" << endl; }
-	~Base3() { cout << "Destructing Base3\t*" << endl; }
+	Base2(int var) :Base0(var) {}
+	int var2;
 };
-class Derived :public Base2, public Base1, public Base3 {
+class Derived :public Base1, public Base2 {
 public:
-	Derived(int a, int b, int c, int d) :Base1(a), m2(d), m1(c), Base2(b) {}
-private:
-	Base1 m1;
-	Base2 m2;
-	Base3 m3;
+	Derived(int var):Base0(var),Base1(var),Base2(var){}
+	int var;
+	void fun() {
+		cout << "member of Derived" << endl;
+	}
 };
-
-int main()
-{
-	Derived obj = Derived(1, 2, 3, 4);
+int main() {
+	Derived d(1);
+	d.var = 2;
+	d.fun();
+	
 	return 0;
 }
